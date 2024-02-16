ca80d974777d1bc461be48ad04810c4445a6131b
conniekeem <143431861+conniekeem@users.noreply.github.com>
2024-02-16 02:57:42 -0500

    copied hw 3 to 4 and added the different links

---
+++ solution-hw4/app.js
@@ -0,0 +1,107 @@
+// First, we get the query string from the URL. This is the list of parameters
+// that begins with a question mark. (These are known as "search parameters")
+const queryString = window.location.search;
+
+console.log(queryString);
+
+// Then, we use the query string to create a URLSearchParams object:
+const params = new URLSearchParams(queryString);
+
+console.log(params);
+
+// Finally, we can access the parameter we want using the "get" method:
+const rollType = params.get('roll')
+
+console.log(rollType);
+
+/* ------------------------------------------------------------------------- */
+
+// Now, we will use the URL parameter to update our page.
+
+// Update the header text
+const headerElement = document.querySelector('#rollsHeader'); //or change to class
+headerElement.innerText = rollType + ' Cinnamon Roll'
+
+let filtered_rollImage = rolls.filter((roll) =>
+    roll.imageFile === ".jpg");
+console.log(filtered_rollImage);
+
+// Update the image
+const rollImage = document.querySelector('#thumbnail');
+rollImage.src = './products/' + rollType.this.imageFile;
+
+//empty cart array
+const cart = [];
+
+// Objects for the glazing options and the packing options
+// each with their own properties of the options that are on dropdown
+const glazingOptions = 
+    {
+      "Keep Original": 0,
+      "Sugar milk": 0,
+      "Vanilla milk": .5,
+      "Double chocolate": 1.5,
+    };
+
+const packingOptions = 
+  {
+    "1": 1,
+    "3": 3,
+    "6": 5,
+    "12": 10,
+  };
+
+//find the glazingoptions class on html
+let selectGlazingElement = document.querySelector("#glazingOptions");
+let selectPackingElement = document.querySelector("#packingOptions");
+
+//loop through the object and add it to the options element 
+//to create the dropdown feature with the names of the objects
+for (const[key, value] of Object.entries(glazingOptions)) {
+  let option = document.createElement("option");
+  option.text = `${key}`;
+  option.value = `${value}`;
+  selectGlazingElement.add(option);
+}
+
+for (const[key, value] of Object.entries(packingOptions)) {
+  let option = document.createElement("option");
+  option.text = `${key}`;
+  option.value = `${value}`;
+  selectPackingElement.add(option);
+}
+
+//display the updated price from the options and their price adaptations
+function displayPrice(newPrice) {
+  let priceTextElement = document.querySelector(".productPrice");
+  priceTextElement.innerText = newPrice;
+}
+
+//have a constant variable for the base price
+//initialize the base price so that it would load with the page
+const basePrice = 2.49;
+displayPrice("$"+basePrice);
+
+//changing the price value depending on the adaptations
+function onSelectValueChange() {
+    // console.log("You selected this glaze: " + this.glazingOptions.value);
+		// console.log("You selected this pack: " + this.packingOptions.value);
+
+    //find and change the value of each option object into a float
+    let glazingPrice = parseFloat(this.glazingOptions.value);
+    let packPrice = parseFloat(this.packingOptions.value);  
+
+    //update/initialize the current price 
+    let currentPrice = "$"+basePrice;
+
+    //price calculations and display it through the current price vari
+    let priceTotal = ((basePrice + glazingPrice) * packPrice).toFixed(2); 
+    currentPrice = "$"+priceTotal;
+    // console.log("priceTotal: " + priceTotal, "basePrice: "+ basePrice, "packPrice: "+ packPrice, "glazingPrice: "+ glazingPrice);
+    //display the updated price
+    displayPrice(currentPrice);
+}
+
+//add the eventlistener when it changes options and start the function
+selectGlazingElement.addEventListener("onchange", onSelectValueChange);
+selectPackingElement.addEventListener("onchange", onSelectValueChange);
