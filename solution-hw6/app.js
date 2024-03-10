7dbc14320e7ca113301762e4e7fb2c52bdd21f83
conniekeem <143431861+conniekeem@users.noreply.github.com>
2024-03-10 00:49:24 -0500

    added solution 6 and added local storage

---
+++ solution-hw6/app.js
@@ -0,0 +1,167 @@
+// query string from the URL that begins with a question mark
+const queryString = window.location.search;
+// console.log(queryString);
+
+// use query string to create a URLSearchParams object:
+const params = new URLSearchParams(queryString);
+// console.log(params);
+
+// access the parameter we want using the "get" method:
+const rollType = params.get("roll")
+// console.log(rollType);
+
+/* ------------------------------------------------------------------------- */
+// use the URL parameter to update our page.
+
+// Update the header text
+const headerElement = document.querySelector("#rollsHeader");
+headerElement.innerText = rollType + " Cinnamon Roll";
+
+// Update the image
+const rollImage = document.querySelector(".productThumbnail");
+const rollImageFile = rolls[rollType]["imageFile"];
+// console.log(rollImageFile);                  
+rollImage.src = "./assets/products/" + rollImageFile;    
+
+/* ------------------------------------------------------------------------- */
+// glazing and packing options adaptations
+
+// Objects for the glazing options and the packing options
+// each with their own properties of the options that are on dropdown
+const glazingOptions = 
+    {
+      "Keep Original": 0,
+      "Sugar milk": 0,
+      "Vanilla milk": .5,
+      "Double chocolate": 1.5,
+    };
+
+const packingOptions = 
+  {
+    "1": 1,
+    "3": 3,
+    "6": 5,
+    "12": 10,
+  };
+
+//find the glazingoptions class on html
+let selectGlazingElement = document.querySelector("#glazingOptions");
+let selectPackingElement = document.querySelector("#packingOptions");
+
+//loop through the object and add it to the options element 
+//to create the dropdown feature with the names of the objects
+for (const[key, value] of Object.entries(glazingOptions)) {
+  let option = document.createElement("option");
+  option.text = `${key}`;
+  option.value = `${value}`;
+  selectGlazingElement.add(option);
+}
+
+for (const[key, value] of Object.entries(packingOptions)) {
+  let option = document.createElement("option");
+  option.text = `${key}`;
+  option.value = `${value}`;
+  selectPackingElement.add(option);
+}
+
+//display the updated price from the options and their price adaptations
+function displayPrice(newPrice) {
+  let priceTextElement = document.querySelector(".productPrice");
+  priceTextElement.innerText = newPrice;
+}
+
+//have a constant variable for the base price and find the new data from the rollsdata.js
+//initialize the base price so that it would load with the page
+const basePrice = rolls[rollType]["basePrice"];
+displayPrice("$"+basePrice);
+
+//changing the price value depending on the adaptations
+function onSelectValueChange() {
+    // console.log("You selected this glaze: " + this.glazingOptions.value);
+		// console.log("You selected this pack: " + this.packingOptions.value);
+
+    //find and change the value of each option object into a float
+    let glazingPrice = parseFloat(this.glazingOptions.value);
+    let packPrice = parseFloat(this.packingOptions.value);  
+
+    //update/initialize the current price 
+    let currentPrice = "$"+basePrice;
+
+    //price calculations and display it through the current price vari
+    let priceTotal = ((basePrice + glazingPrice) * packPrice).toFixed(2); 
+    currentPrice = "$"+priceTotal;
+    // console.log("priceTotal: " + priceTotal, "basePrice: "+ basePrice, "packPrice: "+ packPrice, "glazingPrice: "+ glazingPrice);
+    //display the updated price
+    displayPrice(currentPrice);
+}
+
+//add the eventlistener when it changes options and start the function
+selectGlazingElement.addEventListener("onchange", onSelectValueChange);
+selectPackingElement.addEventListener("onchange", onSelectValueChange);
+
+/* ------------------------------------------------------------------------- */
+// adding cart implementation 
+
+//new class for the rolls added to the cart
+class Roll {
+  constructor(rollType, rollGlazing, packSize, basePrice) {
+      this.type = rollType;
+      this.glazing =  rollGlazing;
+      this.size = packSize;
+      this.basePrice = basePrice;
+
+      this.element = null;
+}
+}
+
+//empty cart
+const cart = [];
+
+//function to update the cart with the rolls we added
+function updateCart() {
+  let glazingName = selectGlazingElement.options[selectGlazingElement.selectedIndex].text;
+  let packingName = selectPackingElement.options[selectPackingElement.selectedIndex].text;
+  //add the rolls to the cart array
+  cart.push(new Roll(rollType, glazingName, packingName, basePrice));
+  console.log(cart);
+}
+
+//make the button activate the function
+const btnCart = document.querySelector(".productButton");
+btnCart.onclick = updateCart;
+
+/**** ADD TO CART ***************************************************/
+
+function addCart() {
+  // Nothing here yet!
+  // console.log("Submitted Note!")
+  const inputImageURL = document.querySelector("#note-editor-image");
+  const inputTitle = document.querySelector("#note-editor-title");
+  const inputBody = document.querySelector('#note-editor-body');
+  
+  const newNoteCard = addNewNote(inputImageURL.src, inputTitle.value, inputBody.value);
+  createElement(newNoteCard);
+  saveToLocalStorage();
+  console.log(newNoteCard);
+}
+
+function saveToLocalStorage() {
+  // transfer set into a array BECAUSE JSON CAN ONLY GET ARRAY THAT IS INDEXED
+  const cartArray = Array.from(notecardSet);
+  const notecardArrayString = JSON.stringify(notecardArray);
+  localStorage.setItem('storedNotes', notecardArrayString);
+  // console.log(notecardArrayString);
+}
+
+function retrieveFromLocalStorage() {
+  const notecardArrayString = localStorage.getItem('storedNotes');
+  const notecardArray = JSON.parse(notecardArrayString);
+  for (const noteData of notecardArray) {
+    const notecard = addNewNote(noteData.noteImageURL, noteData.noteTitle, noteData.noteBody);
+    createElement(notecard);
+  }
+}
+
+if (localStorage.getItem('storedNotes') != null){ 
+  retrieveFromLocalStorage();
+} 
---
+++ solution-hw6/cart.js
@@ -0,0 +1,162 @@
+//new class for the rolls added to the cart
+class Roll {
+  constructor(rollType, rollGlazing, packSize, basePrice) {
+      this.type = rollType;
+      this.glazing =  rollGlazing;
+      this.size = packSize;
+      this.basePrice = basePrice;
+
+      this.element = null;
+}
+}
+
+//mew set to contain all the rolls in the cart
+const cartSet = new Set();
+
+//function to adding a new roll to set
+function addNewRoll(rollType, rollGlazing, packSize, basePrice) {
+  const roll = new Roll(rollType, rollGlazing, packSize, basePrice);
+  cartSet.add(roll);
+  return roll;
+}
+
+//have to create NEW html element for the NEW roll by creating a template
+function createElement(roll){
+  //find the html element that can be the new template for these new rolls
+  const template = document.querySelector("#rollTemplate");
+  const clone = template.content.cloneNode(true);
+
+  //clone that specific class
+  roll.element = clone.querySelector(".rollItem");
+  // console.log(roll.element);
+  
+  //have the button remove delete the roll with the function
+  const btnRemove = roll.element.querySelector(".remove");
+  btnRemove.addEventListener("click", () => {
+      deleteRoll(roll);
+  })
+
+  //add them to the div cartitem class
+  const cartListElement = document.querySelector(".cartItem");
+  cartListElement.prepend(roll.element);
+
+  //and udpate the elements within this cloned/template roll with new properties
+  updateElement(roll);
+}
+
+// Objects for the glazing options and the packing options
+// each with their own properties
+const glazingOptions = 
+  {
+    "Keep original": 0,
+    "Sugar milk": 0,
+    "Vanilla milk": .5,
+    "Double chocolate": 1.5,
+  };
+
+const packingOptions = 
+  {
+    "1": 1,
+    "3": 3,
+    "6": 5,
+    "12": 10,
+  };
+
+  //function to update each element in the template of the roll
+function updateElement(roll) {
+  //updating each element
+  const rollImageElement = roll.element.querySelector(".cartThumbnail");
+  const rollNameElement = roll.element.querySelector(".rollName");
+  const rollGlazeElement = roll.element.querySelector(".rollGlaze");
+  const rollPackElement = roll.element.querySelector(".rollPack");
+  const rollPriceElement = roll.element.querySelector(".cartPrice");
+
+  // console.log(typeof(roll.glazing));
+  // console.log(glazingOptions[roll.glazing]);
+
+  //getting the values of each glaze and price of each roll
+  let glazingPrice = parseFloat(glazingOptions[roll.glazing]);
+  // console.log(glazingPrice);
+  let packPrice = parseFloat(packingOptions[roll.size]);
+  // console.log(packPrice);
+
+  //re-calcuating the price of each roll with their options
+  let rollItemPrice = ((roll.basePrice + glazingPrice) * packPrice).toFixed(2);
+
+  //assigning each elements to the respective rolls
+  rollImageElement.src = "./assets/products/" + rolls[roll.type]["imageFile"];
+  rollNameElement.innerText = roll.type + " Cinnamon Roll";
+  rollGlazeElement.innerText = roll.glazing;
+  rollPackElement.innerText = "Pack Size: " + roll.size;
+  rollPriceElement.innerText = "$" + rollItemPrice;
+}
+
+//deleting the roll with the remove button from the set and html
+function deleteRoll(roll){
+  // have to remove the notecard element AND the set
+  roll.element.remove();
+  cartSet.delete(roll);
+  //keep updating even when removing
+  updateTotalPrice();
+}
+
+//udpating the total price of each roll and their options price adaptations
+function updateTotalPrice() {
+  const cartTotalElement = document.querySelector(".totalPrice");
+  //setting the total price
+  let totalPrice = 0;
+
+  // console.log(cartSet.size);
+
+  //looping through the rolls to get each of their price again
+  //and summing them all
+  for (const roll of cartSet) {
+      let glazingPrice = parseFloat(glazingOptions[roll.glazing]);
+      let packPrice = parseFloat(packingOptions[roll.size]);
+
+      let rollPrice = ((roll.basePrice + glazingPrice) * packPrice).toFixed(2);
+      // console.log(rollPrice);
+      totalPrice += parseFloat(rollPrice);
+      // console.log(totalPrice);
+  }
+  //if we removed all the rolls from set, then make total 0
+  if (cartSet.size === 0) {
+    totalPrice = 0.00;
+  }
+
+  //display it
+  cartTotalElement.innerText = "$" + (totalPrice).toFixed(2);
+}
+
+//implement the 4 rolls
+const appleRoll = addNewRoll(
+  "Apple",
+  "Keep original",
+  "3",
+  rolls["Apple"]["basePrice"]
+);
+const raisinRoll = addNewRoll(
+  "Raisin",
+  "Sugar milk",
+  "3",
+  rolls["Raisin"]["basePrice"]
+);
+const walnutRoll = addNewRoll(
+  "Walnut",
+  "Vanilla milk",
+  "12",
+  rolls["Walnut"]["basePrice"]
+);
+const originalRoll = addNewRoll(
+  "Original",
+  "Sugar milk",
+  "1",
+  rolls["Original"]["basePrice"]
+);
+
+//need to iterate all the objects in the set
+for (const roll of cartSet) {
+  createElement(roll);
+  //keep updating the price for each added roll
+  updateTotalPrice();
+}
